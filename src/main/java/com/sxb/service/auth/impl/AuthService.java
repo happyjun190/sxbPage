d5af1e63dd97828f47bc027da5249c531a81a2a7
shenjun-company <shenjun@126.com>
2016-05-26 15:17:39 +0800

    test

--- src/main/java/com/sxb/service/auth/impl/AuthService.java
+++ src/main/java/com/sxb/service/auth/impl/AuthService.java
@@ -20,9 +20,11 @@ public class AuthService implements IAuthService {
 	
 	@Override
 	public JsonResult login(Map<String, Object> map) {
-		
+		String password = (String) map.get("password");
 		TabUser tabUser = userDAO.getUserInfo(map);
-		
+		if(tabUser==null||!tabUser.getPassword().equals(password)) {
+			return new JsonResult(ReturnCode.PARAMSERROR,"账户名活密码错误,请重新登陆",null);
+		}
 		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("userInfo", tabUser);
 		
